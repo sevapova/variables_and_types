da273a7ac273d45d8eb4b008d8d80aeb6d6af075
sevapova <slatipova99@gmail.com>
2024-10-18 14:01:26 +0300

    misol 6

--- begin06.py
+++ begin06.py
@@ -7,3 +7,11 @@
 # Find the result of that expression 'https://github.com/xakimovallamurod/variables_and_types/blob/main/README.md#begin06';
 
 # # Print the variable 'k'.
+
+a =24 #1 usul
+b= 3
+c= 5
+k= (a,b,c)
+
+print24=(k)
+
